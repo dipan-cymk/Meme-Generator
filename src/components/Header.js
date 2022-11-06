49c85ac999da0272d896122d2016e6e655d04dda
unknown <dipanshua4@gmail.com>
2022-11-06 16:53:57 +0530

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Header()
+{
+return(
+    <header className="header">
+        <img
+         src="./images/troll-face.png"
+         className="header--image"
+         />
+        <h2 classname="header--title">Meme Generator</h2>
+        <h4 className="header--project">Dipanshu Garg</h4>
+    </header>
+)
+}
