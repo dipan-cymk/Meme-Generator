a8d0d6bc47f97b2fcdfba86d95f827823e072820
unknown <dipanshua4@gmail.com>
2022-11-06 17:31:50 +0530

    Updated Memes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,14 @@
 import React from "react"
-
-export default function Header()
-{
-return(
-    <header className="header">
-        <img
-         src="./images/troll-face.png"
-         className="header--image"
-         />
-        <h2 classname="header--title">Meme Generator</h2>
-        <h4 className="header--project">Dipanshu Garg</h4>
-    </header>
-)
+import imag from "../images/troll-face.png"
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src={imag} 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">YASH RAJPUROHIT</h4>
+        </header>
+    )
 }
